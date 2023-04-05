323e3605fcbb2e26c57a39242e07d172e3946eff
tkumarct <tkumar@ctepl.com>
2023-04-05 13:14:32 +0530

    sensor_appdfsf

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -16,6 +16,6 @@ def load_sensor_data():
         with(open(sensor_file)) as data_file:          
             data_reader=csv.DictReader(data_file, delimiter=',')
             #Load Data Records
-            for row in data_file:
+            for row in data_reader:
                 sensor_data.append(row)
     return sensor_data
