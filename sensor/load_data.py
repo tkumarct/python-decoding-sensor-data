650aeb3db36dff459024a352c7d573eef4fea37a
tkumarct <tkumar@ctepl.com>
2023-04-05 13:11:34 +0530

    sensor_app

---
+++ sensor/load_data.py
@@ -0,0 +1,21 @@
+#Import os, glob, and csv
+import os
+import glob
+import csv
+
+#Create a Function to Parse the Data
+def load_sensor_data():
+    sensor_data=[]
+    #Sensor Data File Management
+    sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
+    cwd = os.getcwd()
+    print(cwd)
+    print(sensor_files)
+    #Read from data
+    for sensor_file in sensor_files:
+        with(open(sensor_file)) as data_file:          
+            data_reader=csv.DictReader(data_file, delimiter=',')
+            #Load Data Records
+            for row in data_file:
+                sensor_data.append(row)
+    return sensor_data
