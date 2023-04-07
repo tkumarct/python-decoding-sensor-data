9f2c018943a74a999b8f66ea742b410d15653b9a
tkumarct <tkumar@ctepl.com>
2023-04-07 12:33:42 +0530

    temp_new

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -28,11 +28,11 @@ temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
-
+     
+# Module 4 code here:
 recs = temperature_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Temperature sensor records for date: {} = {}".format(
-    test_date.strftime("%m/%d/%y"), len(recs))
-print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))      
-# Module 4 code here:
+    test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs))) 
 
 # Module 5 code here:
--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -1,4 +1,5 @@
 from house_info import HouseInfo
+from datetime import date
 
 class TemperatureData(HouseInfo):
     def _convert_data(self,data):
